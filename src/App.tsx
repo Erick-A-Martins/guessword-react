bb3d4e6847e3911105867364f2c7672403f47484
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-02-10 08:58:41 -0300

    feat: display word and used letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,51 @@
+import styles from "./app.module.css";
+import { WORDS, type Challenge } from "./utils/words";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
 import { Button } from "./components/Button";
-import { LettersUsed } from "./components/LettersUsed";
-import { Input } from "./components/Input";import styles from "./app.module.css";
+import { LettersUsed, type LettersUsedProps } from "./components/LettersUsed";
+import { Input } from "./components/Input";
+import { useEffect, useState } from "react";
 
 export default function App(){
+    const [letter, setLetter] = useState("");
+    const [attempts, setAttempts] = useState(0);
+    const [lettersUsed, setLetterUsed] = useState<LettersUsedProps[]>([]);
+    const [challenge, setChallenge] = useState<Challenge | null>(null);
 
     function handleRestartGame(){
         alert("Reiniciar o jogo!");
     }
 
+    function startGame(){
+        const index = Math.floor(Math.random() * WORDS.length);
+        const randomWord = WORDS[index];
+        
+        setChallenge(randomWord);
+        setAttempts(0);
+        setLetter("");
+    }
+
+    useEffect(() => {
+        startGame()
+    }, [])
+
+    if(!challenge){
+        return;
+    }
+
     return (
         <div className={styles.container}>
             <main>
-                <Header current={5} max={10} onRestart={handleRestartGame} />
+                <Header current={attempts} max={10} onRestart={handleRestartGame} />
                 
                 <Tip tip="Uma das linguagens de programação mais utilizadas" />
 
                 <div className={styles.word}>
-                    <Letter value="R" />
-                    <Letter value="E" />
-                    <Letter value="A" />
-                    <Letter value="C" />
-                    <Letter value="T" />
+                    {challenge.word.split("").map((letter) => (
+                            <Letter value={letter} />
+                    ))}
                 </div>
 
                 <h4>Palpite</h4>
@@ -33,7 +55,7 @@ export default function App(){
                     <Button title="Confirmar" />
                 </div>
 
-                <LettersUsed />
+                <LettersUsed data={lettersUsed} />
             </main>
         </div>
     )
--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,16 +1,26 @@
 import styles from "./styles.module.css";
 import { Letter } from "../Letter";
 
-// type Props
+export type LettersUsedProps = {
+    value: string
+    correct: boolean
+}
 
-export function LettersUsed(){
+type Props = {
+    data: LettersUsedProps[]
+}
+
+export function LettersUsed({ data }: Props){
     return (
         <div className={styles.lettersUsed}>
             <h5>Letras utilizadas</h5>
 
             <div>
-                <Letter value="X" size="small" color="correct"/>
-                <Letter value="E" size="small" color="wrong"/>
+                {
+                    data.map(({ value, correct }) => ( 
+                        <Letter value={value} size="small" color={correct ? "correct" : "wrong"}/>
+                    ))
+                }
             </div>
         </div>
     )
